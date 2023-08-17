de09a2208af0e7295d0087d9a406b3912b9a6423
ethan-yang <89763726+Tofyu@users.noreply.github.com>
2023-08-16 20:50:19 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,75 +3,91 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import LoginScreen from './screens/LoginScreen';
+import CommunityLogInScreen from './screens/CommunityLogInScreen';
 import ResRegisterScreen from './screens/ResRegisterScreen';
 import ResProfileScreen from './screens/ResProfileScreen';
 import CommunityAddUserScreen from './screens/CommunityAddUserScreen';
 import CommunityRegisterScreen from './screens/CommunityRegisterScreen';
 import CommunityProfileScreen from './screens/CommunityProfileScreen';
-import CommunityEventRequestScreen from './screens/CommunityEventRequestScreen' //need to be made
+import CommunityEventRequestScreen from './screens/CommunityEventRequestScreen' 
 import CommunityManageUsersScreen from './screens/CommunityManageUsersScreen'
-import CommunityManagerHomeScreen from './screens/CommunityManagerHomeScreen'    //need to be made
+import CommunityManagerHomeScreen from './screens/CommunityManagerHomeScreen'    
 import CommunityEventsScreen from './screens/CommunityEventsScreen'
 import ResFeedbackScreen from './screens/ResFeedbackScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const ProfileStack = () => {
+const EventsStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Resident Profile" component={ResProfileScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Volunteer Profile" component={VolProfileScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Community Events" component={CommunityEventsScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Feedback" component={ResFeedbackScreen} options={{ headerShown: false }} />
+   
     </Stack.Navigator>
   );
 };
 
-const EventStack = () => {
+const ManagerStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Community Events" component={CommunityEventsScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Resident Feedback" component={ResFeedbackScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Volunteer Feedback" component={VolFeedbackScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Manager Home" component={CommunityManagerHomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Manage Users" component={CommunityManageUsersScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Community Register" component={CommunityRegisterScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Event Request" component={CommunityEventRequestScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Community Profile" component={CommunityProfileScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Add Users" component={CommunityAddUserScreen} options={{ headerShown: false }} />
+
+
     </Stack.Navigator>
   );
 };
+
+const BottomTab = () =>{
+  const Tab = createBottomTabNavigator();
+
+  return(
+    <Tab.Navigator>
+        <Tab.Screen name="Events" component={EventsStack} options={{
+          headerShown: false,
+          tabBarLabel: 'Events',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="calendar-check" color={color} size={size} />
+          ),
+        }}
+        />
+        <Tab.Screen name="Profile" component={ResProfileScreen} options={{
+          headerShown: false,
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+        />
+        <Tab.Screen name="Manager" component={ManagerStack} options={{
+          headerShown: false,
+          tabBarLabel: 'Manager Page',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-tie" color={color} size={size} />
+          ),
+        }}
+        />
+      </Tab.Navigator>
+  )
+}
+
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Resident Register" component={ResRegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Volunteer Register" component={VolRegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Community Events" component={TabNavigator} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-
-          if (route.name === 'Events') {
-            iconName = 'calendar';
-          } else if (route.name === 'Profile') {
-            iconName = 'account';
-          }
-
-          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-        },
-      })}
-    >
-      <Tab.Screen name="Events" component={EventStack} />
-      <Tab.Screen name="Profile" component={ProfileStack} />
-    </Tab.Navigator>
+    <Stack.Navigator>
+    <Stack.Screen name="Login" component={CommunityLogInScreen} options={{headerShown:false}}/>
+    <Stack.Screen name="Register" component={ResRegisterScreen} options={{ headerShown: false }}/>
+    <Stack.Screen name="User BottomTab" component={BottomTab} options={{headerShown:false}}/>
+  </Stack.Navigator>
+  </NavigationContainer>
   );
 };
 
