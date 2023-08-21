4bab84936eea6757244e23b95f0db9119b5710b4
ethan-yang <89763726+Tofyu@users.noreply.github.com>
2023-08-20 22:01:35 -0700

    Update CommunityEventsScreen.js

--- screens/CommunityEventsScreen.js
+++ screens/CommunityEventsScreen.js
@@ -1,146 +1,134 @@
-import { StyleSheet,TextInput, View, FlatList, SafeAreaView} from 'react-native'
-import React, { useState, useEffect } from 'react'
-import { collection, getDocs, getDoc, doc, query, where } from "firebase/firestore";
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import { collection, getDocs, getDoc, doc, query, where } from 'firebase/firestore';
 import { db, auth } from '../firebase';
 import { Avatar, Card, IconButton, Button } from 'react-native-paper';
 
-const CommunityEventsScreen = ( {navigation} ) => {
-    const [events, setEvents] = useState([])
-    const user = auth.currentUser;
- 
+const CommunityEventsScreen = ({ navigation }) => {
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [pastEvents, setPastEvents] = useState([]);
+  const user = auth.currentUser;
 
-const getCommunityGroupID = async () => {
-  const docRef = doc(db, "users", user.uid);
-  const docSnap = await getDoc(docRef);
-  let groupID = ""
-  if (docSnap.exists()) {
-    groupID = docSnap.data().communityID
-  } else {
-    console.log("No such document!");
-  }
-  console.log(groupID, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-  return groupID
-}
+  const getCommunityGroupID = async () => {
+    const docRef = doc(db, 'users', user.uid);
+    const docSnap = await getDoc(docRef);
+    let groupID = '';
 
-    useEffect(()=>{
-      fetchData();
-      
-    }, [])
+    if (docSnap.exists()) {
+      groupID = docSnap.data().communityID;
+    } else {
+      console.log('No such document!');
+    }
+
+    return groupID;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let eventsFromDB = [];
+    const groupID = await getCommunityGroupID();
+    const eventsQuery = query(collection(db, 'events'), where('communityID', '==', groupID));
+    const querySnapshot = await getDocs(eventsQuery);
+    const docsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   
-    const Item = ({ name }) => (
-      <View style={{ padding: 20 }}>
-        <Text>{name}</Text>
-      </View>
-    );
-    
-    const SectionHeader = ({ dateTime }) => (
-      <View style={{ backgroundColor: '#f2f2f2', padding: 10 }}>
-        <Text>{dateTime}</Text>
-      </View>
-    );
-
-    const fetchData = async () => {
-      let eventsFromDB = [];
-      const groupID = getCommunityGroupID()
-      const query = query(collection(db, "events"), where( communityID, "==", groupID));
-      const querySnapshot = await getDocs(query);
-      const docsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      console.log("+++++++++++++++++++++++++++++++++++", docsData);
-
-      for (const docSnap of docsData) {
-        if (!docSnap.empty && docSnap.communityID == groupID) { // Check if the groupID matches
-          const songsCollectionRef = collection(db, 'events', docSnap.id, 'songs');
-          const songsSnapshot = await getDocs(songsCollectionRef);
-          const songsData = songsSnapshot.docs.map((songDoc) => songDoc.data());
-    
-          eventsFromDB.push({ id: docSnap.id, ...docSnap, songsData });
-          console.log("Document:", docSnap);
-        } else {
-          console.log(docSnap.communityID, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-          console.log("No such document or event has a different groupID, skipping:", docSnap);
-        }
+    for (const docSnap of docsData) {
+      if (docSnap.communityID === groupID) {
+        const songsCollectionRef = collection(db, 'events', docSnap.id, 'songs');
+        const songsSnapshot = await getDocs(songsCollectionRef);
+        const songsData = songsSnapshot.docs.map((songDoc) => songDoc.data());
+  
+        eventsFromDB.push({ id: docSnap.id, ...docSnap, songsData });
       }
-    
-      console.log("Events:", eventsFromDB);
-        
-        console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%", eventsFromDB)
+    }
+  
+    const sortedData = eventsFromDB.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
+  
+    const currentDateTime = new Date();
+    const upcoming = [];
+    const past = [];
+  
+    for (const event of sortedData) {
+      const eventDateTime = event.dateTime.toDate();  
 
-          // Todo: group by date
-          // const groupedData = eventsFromDB.reduce((acc, cur) => {
-          //   let options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-          //   const dateTime = cur.dateTime.toDate().toLocaleString(undefined, options); // format to date only;
-          //   const item = { id: cur.id, name: cur.volunteerGroupName, time: cur.dateTime.toDate().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }), location: cur.communityName };
-          //   const group = acc.find(group => group.dateTime === dateTime);
-          //   if (!group) {
-          //     acc.push({ dateTime, data: [item] });
-          //   } else {
-          //     group.data.push(item);
-          //   }
-          //   return acc;
-          // }, []);
+      if (eventDateTime <= currentDateTime) {
+        past.push(event);
+      } else {
+        upcoming.push(event);
+      }
+    }
+  
+    const sortedUpcoming = upcoming.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime));
+    const sortedPast = past.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
+  
+    setUpcomingEvents(sortedUpcoming);
+    setPastEvents(sortedPast);
+  };
+  
 
-          // Sort by date
-        const sortedData = eventsFromDB.sort((a, b) => new Date(a.dateTime) - new Date(b.dateTime)); 
+  const renderItem = ({ item }) => {
+    const currentDateTime = new Date();
+    const eventDateTime = item.dateTime.toDate();
+    let buttonToRender;
 
-        console.log('3.2************', sortedData);
-        setEvents(sortedData);
-          
-        console.log('4 useEffect end')
-      };
-
-
-      const renderItem = ({ item }) => (
-        <Card>
-          <Card.Title
-            title= {item.volunteerGroupName}
-            subtitle= {item.dateTime.toDate().toLocaleString([], { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}
-            left={(props) => <Avatar.Icon {...props} icon="folder" />}
-            right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
-          />
-          <Card.Actions>
-          <Button  onPress={() => navigation.navigate('Feedback', {
-             eventID: item.id
-          })}>Send Feedback</Button>
-        </Card.Actions>
-        </Card>
-
-      //   <View style={styles.item}>
-      //     <Text style={styles.label}>{item.volunteerGroupName}</Text>
-      //     <Text style={styles.subLabel}>Date:</Text>
-      //     <Text style={styles.info}>{item.dateTime.toDate().toLocaleString([], { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}</Text>
-      //     <Text style={styles.subLabel}>Location:</Text>
-      //     <Text style={styles.info}>{item.communityName}</Text>
-      //     <Text style={styles.subLabel}>Songs:</Text>
-      //     <View style={styles.songsContainer}>
-      //   {item.songsData.map((song, index) => (
-      //     song.isSelected && (
-      //       <Text key={index} style={styles.songName}>{song.name}</Text>
-      //     )
-      //   ))}
-      // </View>
-      //   <Button title="Send feedback" onPress={() => navigation.navigate('Resident Feedback', {
-      //       eventID: item.id
-      //     })} />
-        
-      //   </View>
+    if (eventDateTime <= currentDateTime) {
+      buttonToRender = (
+        <Button onPress={() => navigation.navigate('Feedback', { eventID: item.id })}>
+          Send Feedback
+        </Button>
       );
-      
+    } else {
+      buttonToRender = (
+        <Button onPress={() => navigation.navigate('Event Details', { eventID: item.id })}>
+          Event Details
+        </Button>
+      );
+    }
+
     return (
+      <Card>
+        <Card.Title
+          title={item.volunteerGroupName}
+          subtitle={eventDateTime.toLocaleString([], {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          })}
+        />
+        <Card.Actions>{buttonToRender}</Card.Actions>
+      </Card>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Welcome Text */}
+      <Text style={styles.welcomeText}>Welcome, {user.displayName}!</Text>
       
-      <View style={styles.container}>
-      <FlatList 
-        data={events}
-        keyExtractor={(item, index) => item.id + index}
+      {/* Upcoming Events */}
+      <FlatList
+        data={upcomingEvents}
+        keyExtractor={(item) => item.id}
         renderItem={renderItem}
         contentContainerStyle={styles.listContent}
+        ListHeaderComponent={<Button>Upcoming Events</Button>}
+      />
+      
+      {/* Past Events */}
+      <FlatList
+        data={pastEvents}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContent}
+        ListHeaderComponent={<Button>Past Events</Button>}
       />
     </View>
-      
-    )
-
-  }
-
-export default CommunityEventsScreen
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -149,37 +137,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 20,
   },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
   listContent: {
     paddingBottom: 20,
   },
-  item: {
-    backgroundColor: '#f2f2f2',
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subLabel: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    marginBottom: 5,
-    color: '#666',
-  },
-  info: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  songsContainer: {
-    marginTop: 5,
-  },
-  songName: {
-    fontSize: 14,
-    marginBottom: 3,
-    color: '#333',
-  },
+});
 
-})
+export default CommunityEventsScreen;
